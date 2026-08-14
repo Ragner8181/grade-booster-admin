fcc7af3c615d49a234cf270800bbde50eb68d3ae
Ragner8181 <tsega791.com>
2026-08-14 18:38:10 +0200

    Fix unused React imports for build

--- src/sections/PaymentApprovalsSection.tsx
+++ src/sections/PaymentApprovalsSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { dbSelect, dbUpdate } from '../lib/db';
 import { supabase } from '../lib/supabase';
 
--- src/sections/StatisticsSection.tsx
+++ src/sections/StatisticsSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { dbSelect } from '../lib/db';
 
 type Profile = {
